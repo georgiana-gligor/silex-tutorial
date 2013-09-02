3dad09cd0130ba231f772d978a0ceef8265ef85c
Georgiana Gligor <georgiana.gligor@gmail.com>
2013-09-02 18:58:37 +0300

    register the MonologServiceProvider

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,7 +2,12 @@
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
+use Silex\Provider\MonologServiceProvider;
 $app = new Silex\Application;
+
+$app->register(new MonologServiceProvider(), array(
+	'monolog.logfile' => __DIR__ . '/../logs/dev.log'
+));
 
 $app
     ->match('/', function () {return 'Up and running';})
