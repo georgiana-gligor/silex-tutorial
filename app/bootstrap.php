df8e7a23814c502adf7b993b6eb3e5cbefe269f1
Georgiana Gligor <georgiana.gligor@gmail.com>
2013-09-02 14:00:55 +0300

    file layout strategy + application bootstrap

---
+++ app/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+$app = new Silex\Application;
+
+// we'll add behaviour here
+
+return $app;
---
+++ web/index.php
@@ -0,0 +1,5 @@
+<?php
+
+$app = require_once(__DIR__ . '/../app/bootstrap.php');
+
+$app->run();
